0c5a25a4918dbe26eaa1aff5ce8f0d4b13b31e56
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-03-31 03:40:02 +0530

    fund_udemy: Numbers: Long and Float data type  is added

---
+++ src/udemy/FloatDataType.kt
@@ -0,0 +1,20 @@
+package udemy
+
+fun main(args:Array<String>){
+
+    var myFlaot : Float = 10.2F
+    var myFloat2 =20.30f
+
+    println("before value $myFlaot and $myFloat2")
+
+    myFlaot = 30f
+    myFloat2 = 40.40F
+
+    println("after init value $myFlaot and $myFloat2")
+
+
+    var myFloat3 : Float = 10F/3F
+    println("the calculation is $myFloat3")
+
+
+}
---
+++ src/udemy/LongDataType.kt
@@ -0,0 +1,20 @@
+package udemy
+
+fun main(args:Array<String>){
+
+    var myLong=0L // defeult it took long data type
+    var myLong1:Long =1
+    var myLong2=1234343
+
+
+    println("before value $myLong and $myLong1 and $myLong2")
+
+
+    myLong =100000
+    myLong1 =20000
+    myLong2=30000
+
+    println("after init value $myLong and $myLong1 and $myLong2")
+
+
+}
