3e0d9560940ed81b70134c775530ee41dc617ec7
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-03-31 00:51:05 +0530

    fund_udemy: Numbers: short and Int data type  is added

---
+++ src/udemy/IntDataType.kt
@@ -0,0 +1,18 @@
+package udemy
+
+fun main(args:Array<String>){
+
+    var myInt1:Int =0
+    var myInt2:Int =1
+    var myInt =0  //bydefault value  we see ctrl+shift+B
+
+    println("before value $myInt1 and $myInt2 and default value is $myInt")
+
+    myInt1 =100
+    myInt2=200
+    myInt =10
+
+    println("after init value $myInt1 and $myInt2 and default value is $myInt")
+
+
+}
---
+++ src/udemy/ShortDataType.kt
@@ -0,0 +1,15 @@
+package udemy
+/*
+* Short 16 bit = 2 byte*/
+fun main(args:Array<String>){
+    var myShort:Short =0
+    var myShort2:Short =1
+    println("before value $myShort and $myShort2")
+
+    // re_assigned value
+    myShort =1000
+    myShort2 = 2000
+
+    println("after init value $myShort and $myShort2")
+
+}
