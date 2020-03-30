80136ded48741c46caff1528d20bb779023496c7
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-03-30 12:57:34 +0530

    fund_udemy: Numbers: byte data type is added

---
+++ src/fundamental/CreateVarible.kt
@@ -0,0 +1,12 @@
+package fundamental
+
+fun main(args:Array<String>){
+    var name =""
+    var age =0
+
+    println("before assiged value $name And $age")
+    name = "sainath"
+    age = 5;
+    println("after assiged value: Name is:$name And age is:$age")
+
+}
---
+++ src/udemy/ByteDataType.kt
@@ -0,0 +1,15 @@
+package udemy
+/*
+        * Byte =8 bit = 1 byte
+        * range = -128 to 127 // signed numbers = - + numbers*/
+fun main(args:Array<String>){
+    var myByte:Byte =1
+    var myByte2:Byte =10;
+    println("before value $myByte and $myByte2")
+
+        myByte = 20
+        myByte2 = 120
+       println("after reassiged byte data types value $myByte and $myByte2")
+
+
+}
