111f73cc566a6e28cc6ab417689fd9b542d4e33c
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-04-01 01:33:53 +0530

    fund_udemy: arithmetic operation  member function is added

--- src/udemy/ArithmeticOperators.kt
+++ src/udemy/ArithmeticOperators.kt
@@ -1,8 +1,8 @@
 package udemy
 
 fun main(args:Array<String>){
-    val  number1 =12.5
-    val  number2 =3.5
+    var  number1 =12.5
+    var  number2 =3.5
 
     var result:Double
     //addition
@@ -25,6 +25,14 @@ fun main(args:Array<String>){
     result = number1%number2
     println("number1%number2: $result")
 
+
+    //mumber function
+    var num1=20
+    var num2=30;
+    var sum =number1.plus(number2)
+    sum = (num1+num2).toDouble()
+
+    println("mumber function : $sum")
 
 
 
