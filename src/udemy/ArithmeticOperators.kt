0b8dedf98b5d991cc363cd136f66e6a8f1479493
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-04-01 01:14:22 +0530

    fund_udemy: arithmetic operation test case is added with String concat function

---
+++ src/udemy/ArithmeticOperators.kt
@@ -0,0 +1,31 @@
+package udemy
+
+fun main(args:Array<String>){
+    val  number1 =12.5
+    val  number2 =3.5
+
+    var result:Double
+    //addition
+     result = number1+number2
+    println("number1+number2: $result")
+
+    //subtraction
+    result = number1-number2
+    println("number1-number2: $result")
+
+    //multiplication
+    result = number1*number2
+    println("number1*number2: $result")
+
+    //division
+    result = number1/number2
+    println("number1/number2: $result")
+
+    //modules
+    result = number1%number2
+    println("number1%number2: $result")
+
+
+
+
+}
---
+++ src/udemy/StringConcat.kt
@@ -0,0 +1,21 @@
+package udemy
+
+fun main(args:Array<String>){
+    var s1="Android"
+    var s2 ="Developer"
+
+    val result = s1+s2
+    println("String concat $result")
+
+    val s3="Android"
+    val s4 ="Developer"
+
+    val result1 = s3+s4
+
+    println("String concat $result1")
+
+
+    var myStr = "sai"
+    var finStr = myStr+"nath"
+    println("concat String $finStr")
+}
