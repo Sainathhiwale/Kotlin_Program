11a1aa4ab334eb4525a8872d558b4eb01a8047b0
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-04-02 00:14:41 +0530

    fund_udemy: comparsion and unary increment and decrement test cases are added

---
+++ src/udemy/ComparsionOperators.kt
@@ -0,0 +1,44 @@
+package udemy
+
+fun main(args:Array<String>){
+
+    var a=20
+     var b=30
+    if (a>b){
+        println("a is greater then b")
+    }else{
+        println("b is greater then a")
+    }
+    if (a<b){
+        println("a is less then b")
+    }else{
+        println("b is less then a")
+    }
+    if (a>=b){
+        println("a is >=b")
+    }else{
+        println("b is >=a")
+    }
+    if (a<=b){
+        println("a is <= b")
+    }else{
+        println("b is <= a")
+    }
+    if (10==10){
+        println("true")
+    }else{
+        println("false")
+    }
+    if (a!=b){
+        println("true")
+    }else{
+        println("false")
+    }
+    a=10+20
+    if (a!=b ){
+        println("true")
+    }else{
+        println("false")
+    }
+
+}
---
+++ src/udemy/UnaryIn&DeOperator.kt
@@ -0,0 +1,36 @@
+package udemy
+
+
+fun main(args:Array<String>){
+
+    var a=20
+    var b=-30
+    var c= false
+    var result1 = +a
+    println(result1)
+
+
+    var result2 = +b
+    println(result2)
+
+    var result3 = -b
+    println(result3)
+
+    var result4 = ++a
+    println(result4)
+
+    var result5 = --a
+    println(result5)
+
+
+    var result6 = !c
+    println(result6)
+
+    var result7 = a.dec() //decrement
+    println(result7)
+
+    var result8 = a.inc() //increment
+    println(result8)
+
+
+}
