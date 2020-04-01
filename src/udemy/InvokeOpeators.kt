fe08036d89f92f4b920727d2e011ea255e40bf31
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-04-02 03:00:45 +0530

    fund_udemy: invole() member is changed with multiple parameters

--- src/udemy/InvokeOpeators.kt
+++ src/udemy/InvokeOpeators.kt
@@ -3,7 +3,8 @@ package udemy
 fun main(args:Array<String>){
   method1()
     foo1.invoke("sainath")
-    foo2.invoke("hiwale","address")
+    foo2.invoke("hiwale","address",9503746253)
+    foo3.invoke("sainath", "hiwale")
 }
 
 val method1={
@@ -12,8 +13,18 @@ val method1={
 val foo1={ name:String ->
  println("my name is: $name")
 }
-val foo2={ surName:String , address:String ->
-    println("my surname is $surName")
-    println("my address is $address")
+val foo2={ surName:String , address:String, mobileNo:Long->
+
+    var myResult:String = """
+           | my surname is $surName
+           | my address is $address
+           | my mobile no is $mobileNo
+    """.trimIndent()
+    println(myResult.trimMargin())
+}
+
+val foo3={name:String,surName:String ->
+    println("my name is $name" +
+            " $surName")
 
 }
