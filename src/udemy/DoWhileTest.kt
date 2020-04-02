3fb06bdf3a2ba11a482ad6e190de538b8f7e1e60
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-04-03 00:53:26 +0530

    fund_udemy: while and do while sample test cases is added

---
+++ src/udemy/DoWhileTest.kt
@@ -0,0 +1,10 @@
+package udemy
+
+fun  main(args:Array<String>){
+
+    var a=0
+    do {
+        a++
+        println("values of a : $a")
+    }while (a<10)
+}
---
+++ src/udemy/WhileLoopTest.kt
@@ -0,0 +1,10 @@
+package udemy
+
+fun main(args:Array<String>){
+
+    var a=0
+    while (a<10){
+        println("current value for a:$a")
+        a++
+    }
+}
