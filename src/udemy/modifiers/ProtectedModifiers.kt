241416d78ca3c641cc70e85c84e823dd49828bc6
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-04-06 03:26:01 +0530

    fund_udemy:kotlin Protected visibility modifiers demo is added

---
+++ src/udemy/modifiers/ProtectedModifiers.kt
@@ -0,0 +1,21 @@
+package udemy.modifiers
+
+open class ProtectedModifiers {
+    protected  var  a=10
+
+}
+class ChildProtected:ProtectedModifiers(){
+
+    fun getValue():Int{
+        return a
+    }
+    fun display(){
+        println(a)
+    }
+}
+fun main(args:Array<String>){
+
+    val obj = ChildProtected()
+    println(obj.display())
+
+}
