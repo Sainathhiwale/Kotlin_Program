9a76b46aeb4b81f027d13c9b0197c9ed9bbec5b9
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-04-07 04:47:40 +0530

    fund_udemy:data class test case is added for pojo classes

---
+++ src/udemy/data_class/CallDataClass.kt
@@ -0,0 +1,6 @@
+package udemy.data_class
+
+fun main(args:Array<String>){
+    var stdData = StudentInfo("sainath",34,"Pune")
+    println(stdData.toString())
+}
---
+++ src/udemy/data_class/StudentInfo.kt
@@ -0,0 +1,4 @@
+package udemy.data_class
+
+data class StudentInfo(var name:String, var roleNo:Int, var address:String){
+}
