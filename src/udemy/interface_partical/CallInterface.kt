614af280b864b3f5cb7556c843ff42ee4ca2058e
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-04-07 03:57:13 +0530

    fund_udemy:kotlin interface nested and inner class sample test case is added

--- src/udemy/interface_partical/CallInterface.kt
+++ src/udemy/interface_partical/CallInterface.kt
@@ -9,4 +9,18 @@ fun main(args:Array<String>){
     obj.volDown()
     obj.volUp()
     obj.startTv()
+
+    //calling nested class
+    var nestObj = Television.TelevisionUsbPort()
+    nestObj.usbName ="Nokia"
+    nestObj.usbNo =1112
+    println("usb port is ${nestObj.usbNo} and ${nestObj.usbName}")
+    nestObj.usbPlug()
+
+    // acess the inner class data member and function member using constructor
+    // var innerObj = Television.TelevisionTataPort() error because constructor
+    var innerObj = Television().TelevisionTataPort()
+    innerObj.portName ="tata sky"
+    innerObj.portNo = 444
+    innerObj.usbPlugTata()
 }
--- src/udemy/interface_partical/Television.kt
+++ src/udemy/interface_partical/Television.kt
@@ -24,4 +24,23 @@ class Television:Remote {
         println("Remote : television volDown")
 
     }
+    //nested class without create the object we can access the nested class data member and function member
+    class TelevisionUsbPort{
+        var  usbName:String =""
+        var usbNo:Int =0
+
+        fun usbPlug(){
+            println("Nested class: USBPort usb is plugin to tv")
+        }
+    }
+
+    //inner class we can access the outer class data member using inner keyword TAG varible
+   inner class TelevisionTataPort{
+        var  portName:String =""
+        var portNo:Int =0
+
+        fun usbPlugTata(){
+            println("Inner class: Tata usb port is connected to tv")
+        }
+    }
 }
