0e4c33e8309aa2ea601ff133af7452eb3e083443
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-04-08 00:24:36 +0530

    fund_udemy:Null Safety- nullable tyep and non_nullbale is added

---
+++ src/udemy/nullable_nonnullable/NonNullableTest.kt
@@ -0,0 +1,12 @@
+package udemy.nullable_nonnullable
+
+fun main(args:Array<String>){
+
+    var str:String = "hello non nullable"
+
+    str = null
+    
+    println(str)
+
+
+}
---
+++ src/udemy/nullable_nonnullable/NullableTest.kt
@@ -0,0 +1,10 @@
+package udemy.nullable_nonnullable
+
+fun main(args:Array<String>){
+
+    var str:String?="hello nullable safe call"
+
+    str = null
+
+    println(str)
+}
