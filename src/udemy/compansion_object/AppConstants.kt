7e1943febe155b1c80a1ab874032b36f492e4e91
Sainath Hiwale <sainathhiwale2@gmail.com>
2020-04-07 06:15:07 +0530

    fund_udemy:compansion object call using .dot without any object creation just like that java static

---
+++ src/udemy/compansion_object/AppConstants.kt
@@ -0,0 +1,9 @@
+package udemy.compansion_object
+
+class AppConstants {
+
+    companion object{
+        const val USERNAME ="Sainath"
+         var COUNT = 0
+    }
+}
---
+++ src/udemy/compansion_object/CallCompansionObject.kt
@@ -0,0 +1,7 @@
+package udemy.compansion_object
+
+fun main(args:Array<String>){
+    AppConstants.COUNT ++
+    println(AppConstants.COUNT)
+    println(AppConstants.USERNAME)
+}
